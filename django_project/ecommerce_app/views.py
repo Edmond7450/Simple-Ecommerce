ceb2e72c446952862d075aab3fee91c4b8985f5e
Edmond Dantes <58954212+Edmond7450@users.noreply.github.com>
2022-08-26 17:00:27 +0800

    process payment

--- django_project/ecommerce_app/views.py
+++ django_project/ecommerce_app/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render, HttpResponse, redirect, \
-    get_object_or_404, reverse
+from decimal import Decimal
+from django.conf import settings
 from django.contrib import messages
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, reverse
+from django.views.decorators.csrf import csrf_exempt
+from paypal.standard.forms import PayPalPaymentsForm
+
 from .models import Product, Order, LineItem
 from .forms import CartForm, CheckoutForm
 from . import cart
+
 
 # Create your views here.
 
@@ -11,8 +16,8 @@ from . import cart
 def index(request):
     all_products = Product.objects.all()
     return render(request, "ecommerce_app/index.html", {
-                                    'all_products': all_products,
-                                    })
+        'all_products': all_products,
+    })
 
 
 def show_product(request, product_id, product_slug):
@@ -27,13 +32,12 @@ def show_product(request, product_id, product_slug):
 
     form = CartForm(request, initial={'product_id': product.id})
     return render(request, 'ecommerce_app/product_detail.html', {
-                                            'product': product,
-                                            'form': form,
-                                            })
+        'product': product,
+        'form': form,
+    })
 
 
 def show_cart(request):
-
     if request.method == 'POST':
         if request.POST.get('submit') == 'Update':
             cart.update_item(request)
@@ -43,9 +47,9 @@ def show_cart(request):
     cart_items = cart.get_all_cart_items(request)
     cart_subtotal = cart.subtotal(request)
     return render(request, 'ecommerce_app/cart.html', {
-                                            'cart_items': cart_items,
-                                            'cart_subtotal': cart_subtotal,
-                                            })
+        'cart_items': cart_items,
+        'cart_subtotal': cart_subtotal,
+    })
 
 
 def checkout(request):
@@ -54,20 +58,20 @@ def checkout(request):
         if form.is_valid():
             cleaned_data = form.cleaned_data
             o = Order(
-                name = cleaned_data.get('name'),
-                email = cleaned_data.get('email'),
-                postal_code = cleaned_data.get('postal_code'),
-                address = cleaned_data.get('address'),
+                name=cleaned_data.get('name'),
+                email=cleaned_data.get('email'),
+                postal_code=cleaned_data.get('postal_code'),
+                address=cleaned_data.get('address'),
             )
             o.save()
 
             all_items = cart.get_all_cart_items(request)
             for cart_item in all_items:
                 li = LineItem(
-                    product_id = cart_item.product_id,
-                    price = cart_item.price,
-                    quantity = cart_item.quantity,
-                    order_id = o.id
+                    product_id=cart_item.product_id,
+                    price=cart_item.price,
+                    quantity=cart_item.quantity,
+                    order_id=o.id
                 )
 
                 li.save()
@@ -75,12 +79,41 @@ def checkout(request):
             cart.clear(request)
 
             request.session['order_id'] = o.id
+            return redirect('process_payment')
 
             messages.add_message(request, messages.INFO, 'Order Placed!')
             return redirect('checkout')
-
 
     else:
         form = CheckoutForm()
         return render(request, 'ecommerce_app/checkout.html', {'form': form})
 
+
+def process_payment(request):
+    order_id = request.session.get('order_id')
+    order = get_object_or_404(Order, id=order_id)
+    host = request.get_host()
+
+    paypal_dict = {
+        'business': settings.PAYPAL_RECEIVER_EMAIL,
+        'amount': '%.2f' % order.total_cost().quantize(Decimal('.01')),
+        'item_name': 'Order {}'.format(order.id),
+        'invoice': str(order.id),
+        'currency_code': 'USD',
+        'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+        'return_url': 'http://{}{}'.format(host, reverse('payment_done')),
+        'cancel_return': 'http://{}{}'.format(host, reverse('payment_cancelled')),
+    }
+
+    form = PayPalPaymentsForm(initial=paypal_dict)
+    return render(request, 'ecommerce_app/process_payment.html', {'order': order, 'form': form})
+
+
+@csrf_exempt
+def payment_done(request):
+    return render(request, 'ecommerce_app/payment_done.html')
+
+
+@csrf_exempt
+def payment_canceled(request):
+    return render(request, 'ecommerce_app/payment_cancelled.html')
